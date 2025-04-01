4b2f51b4fc3ce960442b358e814499b3533919a5
Nael-bhp <74135606+Nael-bhp@users.noreply.github.com>
2025-04-01 17:48:03 -0400

    Test commit
    Test commit

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -47,6 +47,13 @@ export default function TabLayout() {
           tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
         }}
       />
-    </Tabs>
+    <Tabs.Screen
+    name="statistics"
+    options={{
+      title: 'Statistics',
+      tabBarIcon: ({ color }) => <IconSymbol size={28} name="paperplane.fill" color={color} />,
+    }}
+  />
+</Tabs>
   );
 }
